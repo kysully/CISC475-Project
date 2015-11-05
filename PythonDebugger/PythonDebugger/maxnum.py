4fc7dcdedcecfd2816bcb77b09e9723ecdb84f27
kysully <kmsully@udel.edu>
2015-11-04 22:55:55 -0500

    Added maxnum - user defined function that returns the max of two numbers

---
+++ PythonDebugger/PythonDebugger/maxnum.py
@@ -0,0 +1,8 @@
+# CISC 475 - Group 4
+# maxnum
+# - This code returns the max out of a and b
+# - user defined function for pl/python
+def maxnum(a, b):
+    if a < b:
+        return b
+    return a
