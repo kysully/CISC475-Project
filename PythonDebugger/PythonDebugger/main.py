09eeb1d40c111bbd47c67b2be3bcc90378753108
kysully <kmsully@udel.edu>
2015-12-11 16:30:34 -0500

    Updated comments for turn in. Also updated code to reflect most up to date code in postgres.

--- PythonDebugger/PythonDebugger/main.py
+++ PythonDebugger/PythonDebugger/main.py
@@ -1,4 +1,12 @@
+﻿# CISC475 - Project 4
+# This code sets servers as a main
+# run file which launches the python
+# debugger server, additionally it may
+# be used to import and debug (using pdb)
+# other python functions
 import PythonDebuggerServer
 import maxnum
+import sumnumbers
+import pdb
 
 PythonDebuggerServer.serverListen()
